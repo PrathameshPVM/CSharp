e95c24768dcf7bd87465cadfb4f710e4a3126d4f
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 17:37:40 +0530

    Add files via upload

---
+++ Nested_for2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int i, j;
+            for (i = 1; i < 10; i++)
+            {
+                for (j = 1; j < i; j++)
+                {
+                    Console.Write(j + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
