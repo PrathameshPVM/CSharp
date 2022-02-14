0a828e78f1d2142a60bae8fb42d69ceaf8f94bf9
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 17:48:48 +0530

    Add files via upload

---
+++ Foreach.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;   
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int [3];
+            arr [0] = 11;
+            arr [1] = 22;
+            arr [2] = 33;
+
+            for(int i=0; i<3; i++)
+            {
+                Console.WriteLine("arr = " +arr[i]);
+            }
+            foreach(int i in arr)
+            {
+                Console.WriteLine("arr = " + i);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
