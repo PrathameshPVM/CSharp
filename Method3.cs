29c30a3763cb4656751aa0390794f7bb3a037714
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 19:29:24 +0530

    Add files via upload

---
+++ Method3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //creating a user defined method 
+        //method with parameters parameters and returing no value
+        static void factorial(int num)
+        {
+            int res = 1;
+            int i = 1;
+            while (i <= 10)
+            {
+                res = i * num;
+                Console.WriteLine("{0} * {1} = {2}", num, i, res);
+                i++;
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            int num = 5;
+            factorial(num);
+            Console.ReadLine();
+
+        }
+    }
+}
