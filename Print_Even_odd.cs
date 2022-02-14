966ae73aea15783cf043c4f99a918b9b06a840f5
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 18:46:29 +0530

    Add files via upload

---
+++ Print_Even_odd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                goto Even;
+            Console.WriteLine("Number is Odd");
+            goto End;
+        Even:
+            Console.WriteLine("Number is Even");
+        End:;
+        Console.ReadLine();
+        }
+        
+    }
+}
