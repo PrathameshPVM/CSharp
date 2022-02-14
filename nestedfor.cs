6f6092821a1938500f31331ac38df361c1066645
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 17:21:36 +0530

    Add files via upload

---
+++ nestedfor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int i, j;
+            for(i=1;i<10;i++)
+            {
+                for(j=1;j<10;j++)
+                {
+                    Console.WriteLine(j + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
