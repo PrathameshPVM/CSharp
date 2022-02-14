6818020e91cbe25f48c464423b02c5c4845ac3c9
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 19:26:15 +0530

    Add files via upload

---
+++ Method2a.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+        static void factorial(int num)
+        {
+            int fact = 1;
+            while (num > 0)
+            {
+                fact = fact * num;
+                num--;
+            }
+            Console.WriteLine("fact = " + fact);
+
+        }
+        static void Main(string[] args)
+        {
+            int num=5;
+            
+            Console.WriteLine("Enter a number");
+            factorial(num);
+            Console.ReadLine();
+        }
+    }
+}
