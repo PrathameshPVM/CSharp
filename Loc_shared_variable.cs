53a8106406b0eb0ff880b1dde30a77671920102b
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 19:42:27 +0530

    Add files via upload

---
+++ Loc_shared_variable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static int fact = 2;//shared variable
+        static void factorial()
+        {
+            int fact = 1;//locat variable
+            Console.WriteLine("value of fact local varialbe inside method " +fact);
+
+        }
+        static void Main (string[] args)
+        {
+            factorial();
+            Console.WriteLine("fact inside main  = {0}", fact);
+            Console.ReadLine();
+
+        }
+    }
+}
