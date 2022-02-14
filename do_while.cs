6babef998ec711ffb938463cc3bb0ebcaeadb41d
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 15:10:19 +0530

    Add files via upload

---
+++ do_while.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("i = " +i);
+                i++;
+            }
+            while (i <= 10);
+            Console.ReadLine();
+        }
+    }
+}
