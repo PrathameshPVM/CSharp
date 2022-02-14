34b2ce1d27801cf93a2ee398fb2010b7546e4a3c
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 19:10:35 +0530

    Add files via upload

---
+++ Method1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void displaygreetings()
+        {
+            Console.WriteLine("Happy Valentine Day");
+        }
+        static void Main(string[] args)
+        {
+            displaygreetings();
+            Console.ReadLine();
+        }
+           
+    }
+}
