a28994ec8160d118d24807e98578b22be25fda6e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-14 17:51:20 +0530

    Add files via upload

---
+++ Goto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 4;
+            if (number % 2 == 0) goto Even;
+            Console.WriteLine("no is odd");
+            goto End;
+        Even:
+            Console.WriteLine("no is even");
+        End:;
+
+            Console.ReadLine();
+
+        }
+    }
+}
