112dfc6a3df3e2403b3cd24809c696fb7c0c7066
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-15 16:32:17 +0530

    Add files via upload

---
+++ userinput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accet no. from the user and print sq
+            int num, sq;
+            Console.WriteLine("enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            sq = num * num;
+            Console.WriteLine("Sq = " + sq);
+            Console.ReadLine();
+        }
+    }
+}
