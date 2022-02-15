527f1a6f837e0f6b67ca565ec15376150cd5f39d
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-15 16:51:10 +0530

    Add files via upload

---
+++ volume_rect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept 2 no. and display addtion of 2 no.
+            int num1, num2,num3, res;
+            Console.WriteLine("enter lenght");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter breadth");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter height");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            res = num1 * num2*num3;
+            Console.WriteLine("Volume = " + res);
+            Console.ReadLine();
+        }
+    }
+}
