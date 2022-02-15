89607d1a00278678809f5093efdca32142a9ba32
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-15 18:02:58 +0530

    Add files via upload

---
+++ inp_struct_stu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        struct student
+        {
+            public int rno;
+            public String name;
+            public String course;
+        }
+        public static void Main()
+        {
+            student s1;
+            Console.WriteLine("enter rno");
+            s1.rno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter name");
+            s1.name = Console.ReadLine();
+            Console.WriteLine("enter course");
+            s1.course = Console.ReadLine();
+
+            Console.WriteLine("rno " + s1.rno);
+            Console.WriteLine("name " + s1.name);
+            Console.WriteLine("course " + s1.course);
+
+        }
+    }
+}
