4b3e375e77e886f45cafaf9f0d951919d41396ff
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-15 19:41:05 +0530

    Add files via upload

---
+++ string.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            String b = "Maninder";
+            Console.WriteLine(" value " + b);
+            Console.WriteLine("enter value ");
+            b = Console.ReadLine();
+            Console.WriteLine("value = " + b);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
