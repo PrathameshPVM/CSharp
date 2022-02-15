9776d2c15b1df6e2779112b27e41815881eb3d59
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-15 20:19:17 +0530

    Add files via upload

---
+++ area_circum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            float c, a, r;
+            Console.WriteLine("Enter radius ");
+            r = Convert.ToSingle(Console.ReadLine());
+           
+            c = 2 * 3.14f * r;
+            
+
+            a = 3.14f * r * r;
+           
+
+            Console.WriteLine("Circumfarance= " +c);
+            Console.WriteLine("Enter Area= " +a);
+
+            Console.ReadLine();
+        }
+    }
+}
