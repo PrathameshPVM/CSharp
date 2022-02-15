3dc6818f2377d4fd3a619bfbb935c952d6730329
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-15 19:45:50 +0530

    Add files via upload

---
+++ explicit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+
+        public static void Main()
+        {
+            long longValue = 1234567L;
+            int intValue = (int)longValue;
+            Console.WriteLine("(int) {0} = {1}", longValue, intValue);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
