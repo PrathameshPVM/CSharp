0219979130b8bb175e20eca64e63965398312991
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-15 18:05:13 +0530

    Add files via upload

---
+++ long.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intValue = 1236541623;
+            long longValue = intValue;//implicit conversion from int to long
+            Console.WriteLine("(long) {0} = {1}", intValue, longValue);
+
+        }
+    }
+}
+
