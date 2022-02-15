4dde92d0dfff2701341774a6ec3b8e19b385d216
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-15 16:46:50 +0530

    Add files via upload

---
+++ area_circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept radius and print area and circ of circle
+            int radius;
+            float area, circ;
+            Console.WriteLine("enter radius");
+            radius = Convert.ToInt32(Console.ReadLine());
+            area = (float)3.14f * radius * radius;
+            circ = (float)2 * 3.14f * radius;
+            Console.WriteLine("area " + area);
+            Console.WriteLine("circ " + circ);
+            Console.ReadLine();
+        }
+    }
+}
