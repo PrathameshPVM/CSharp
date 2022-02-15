f8748c46a853067290c5eacbd0d3aa8523893053
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-15 16:40:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept 2 no. and display addtion of 2 no.
+            int num1, num2, res;
+            Console.WriteLine("enter number1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            res = num1 + num2;
+            Console.WriteLine("addition = " + res);
+            Console.ReadLine();
+        }
+    }
+}
