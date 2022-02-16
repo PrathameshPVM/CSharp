fe1b976fb826801bfbe68045f7ed9fb9b679119a
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 22:28:29 +0530

    Add files via upload

---
+++ double_dimension_array _2_3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace double_dimension_array_2_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] grid = { { 5, 4, 3 }, { 2, 1, 0 } };
+
+
+            //displaying the array elements
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("\t " + grid[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+
+    }
+}
