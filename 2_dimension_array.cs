747173d617dd8d96caeb0a3e7daa3bfc12a83161
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 22:09:42 +0530

    Add files via upload

---
+++ 2_dimension_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] arr = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("enter marks");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("\t " + arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
