71086edf0b6bf642a99e548c4a206158e63e2056
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 19:17:38 +0530

    Add files via upload

---
+++ pass_by_out.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pass_by_out
+{
+    class Program
+    {
+        static void passbyout(out int x)
+        {
+            x = 5;
+            x++;
+            Console.WriteLine("value inside passbyref method is " + x);
+        }
+        static void Main()
+        {
+            int k;
+            passbyout(out k);
+            Console.WriteLine("value after calling method in main " + k);
+        }
+    }
+}
