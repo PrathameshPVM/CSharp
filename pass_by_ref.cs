cf3e49b9e23828a0cb3fc44a44f8a7f2412bc05b
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 19:14:13 +0530

    Add files via upload

---
+++ pass_by_ref.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pass_by_ref
+{
+    class Program
+    {
+        static void passbyref(ref int x)
+        {
+            x++;
+            Console.WriteLine("value inside passbyref method is " + x);
+        }
+        static void Main()
+        {
+            int k = 6;
+            passbyref(ref k);
+            Console.WriteLine("value after calling method in main " + k);
+        }
+    }
+}
