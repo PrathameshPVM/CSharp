3521123593925a7c8116141df80e39725a4f2719
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 21:58:42 +0530

    Add files via upload

---
+++ single_dimen_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace single_dimen_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr =new int[3];
+            for(int i=0; i<3; i++)
+            {
+                Console.WriteLine("Enter marks");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i=0; i<arr.Length;i++)
+            {
+                Console.WriteLine("marks " +arr[i]);
+            }
+        }
+    }
+}
+
