8158db1a9c462b6c7618ef35f8a248ba50052a72
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 22:23:26 +0530

    Add files via upload

---
+++ single dimension array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            long[] row = { 0, 1, 2, 3 };
+
+
+            //displaying the array elements
+
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write("\t " + row[j]);
+            }
+            Console.WriteLine();
+
+
+        }
+
+    }
+}
