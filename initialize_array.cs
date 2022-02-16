fa43ee217706f0bafa27f34259cc372a7df822b9
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 22:23:02 +0530

    Add files via upload

---
+++ initialize_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace initialize_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] arr = new long[4] { 0, 1, 2, 3 };
+            for (int j = 0;j<4;j++)
+            {
+                Console.WriteLine("\t" +arr[j]);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
+
