3252b14741b66ac03324be0cef0f5bd54e429e7e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 20:09:14 +0530

    Add files via upload

---
+++ varialbe_lenght_parameter_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varialbe_lenght_parameter_list
+{
+    class Program
+    {
+        static long AddList(params long[] v)
+        {
+            long total,i;
+            for(i=0,total=0; i<v.Length; i++)
+            {
+                total +=v[i];
+            }
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            long x = AddList(1,2,3,4,5);
+            Console.WriteLine("value of x = " +x);
+
+        }
+    }
+}
+
