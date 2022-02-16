222f79fb31777117937bc59b5bb9c96ec0d6cc90
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 21:05:21 +0530

    Add files via upload

---
+++ method_overloading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_overloading
+{
+    class Program
+    {
+        static int add(int a,int b)
+        {
+            return a + b;
+        }
+        static int add(int a,int b,int c)
+        {
+            return a + b + c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("addition of 2 no. " + add(1, 2));
+            Console.WriteLine("addition of 3 no. " +add(1,2, 3));
+        }
+    }
+}
+
