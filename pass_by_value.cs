a9804b6726d3304de2e572b0ef23cbb00d75d229
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-16 19:00:43 +0530

    Add files via upload

---
+++ pass_by_value.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pass_by_value
+{
+    class Program
+    {
+        static void passbyvalue(int x)
+        {
+            x++;
+            Console.WriteLine("Value inside pass by value is {0}",x);
+        }
+        static void Main()
+        {
+            int k = 6;
+            passbyvalue(k);
+            Console.WriteLine(k);
+        }
+    }
+}
