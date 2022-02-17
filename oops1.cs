6e23908b664ccff41273e7d36689fdd209ca8878
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-17 14:06:54 +0530

    Add files via upload

---
+++ oops1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops1
+{
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public float salary;
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.empno = 123;
+            emp.empname = "raju";
+            emp.salary = 12345.645f;
+
+            Console.WriteLine("empno " + emp.empno);
+            Console.WriteLine("empname " + emp.empname);
+            Console.WriteLine("salary " + emp.salary);
+            Console.ReadLine();
+
+        }
+    }
+}
+
