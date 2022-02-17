bfca32c49c3bbc63a065950ffa627100fe6c37b2
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-17 14:18:30 +0530

    Add files via upload

---
+++ oops3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+        int empno;
+        string empname;
+        float salary;
+
+        public void gatdata()
+        {
+            Console.WriteLine("enter employee no");
+            empno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter employee name");
+            empname = Console.ReadLine();
+            Console.WriteLine("enter employee salary");
+            salary = Convert.ToSingle(Console.ReadLine());
+
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("empno " + empno);
+            Console.WriteLine("empname " + empname);
+            Console.WriteLine("salary " + salary);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            emp.gatdata();
+            emp.displaydata();
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
