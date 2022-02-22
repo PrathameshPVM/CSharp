80ee2472576edd9c5913d60dd00667aec2f08583
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-22 12:14:19 +0530

    Add files via upload

---
+++ private_access_specifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace private_access_specifier
+{
+    class Program
+    {
+        private string name = "Shantosh Kumar";
+        private void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            // Accessing private variable  
+            Console.WriteLine("Hello " + program.name);
+            // Accessing private function  
+            program.Msg("Peter Decosta");
+        }
+    }
+}
+
+/*Output:
+
+Hello Shantosh Kumar
+Hello Peter Decosta*/
