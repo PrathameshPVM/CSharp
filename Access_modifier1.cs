f25f5da9493034e1b7e586ab5a3854f792ea19c5
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-22 11:46:01 +0530

    Add files via upload

---
+++ Access_modifier1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_modifier1
+{
+    class PublicTest
+    {
+        public string name = "Raju Kumar";
+        public void msg(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PublicTest publictest = new PublicTest();
+            // Accessing public variable  
+            Console.WriteLine("Hello "+publictest.name);
+            // Accessing public function  
+            publictest.msg("Babu Bhaiya");
+        }
+    }
+}
