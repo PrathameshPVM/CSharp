95fe823e4f75579cb85053309bb842d675a14933
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-22 12:10:33 +0530

    Add files via upload

---
+++ protectedinternalaccessspecifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace protectedinternalaccessspecifier
+{
+    class InternalTest
+    {
+        protected internal string name = "Shantosh Kumar";
+        protected internal void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InternalTest internalTest = new InternalTest();
+            // Accessing protected internal variable  
+            Console.WriteLine("Hello " + internalTest.name);
+            // Accessing protected internal function  
+            internalTest.Msg("Peter Decosta");
+        }
+    }
+}
