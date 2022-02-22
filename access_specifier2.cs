7e40813811c71b998de14b987f31692bfc207fa4
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-22 11:57:07 +0530

    Add files via upload

---
+++ access_specifier2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace access_specifier2
+{
+    class ProtectedTest
+    {
+         protected string name="Shashikant"
+            protected void Msg(string msg)
+        {
+            Console.WriteLine("Hello "+msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProtectedTest protectedTest = new ProtectedTest();
+            Console.WriteLine("Hello " + protectedTest.name);
+            ProtectedTest.Msg("Shyam");
+        }
+    }
+}
+//Compile time error
+//'ProtectedTest.name' is inaccessible due to its protection level.
