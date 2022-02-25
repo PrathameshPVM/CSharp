932de317f6221f1fa95248342252826e0865d21e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-26 01:52:07 +0530

    Add files via upload

---
+++ const_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace const_1
+{
+    public class Program
+    {
+        class book
+        {
+            int bookid;
+            string title,author;
+            int price;
+            public book()
+            {
+                bookid = 1;
+                title = ".Net";
+                author = "Raju";
+                price = 150;
+            }
+            public void display()
+            {
+                Console.WriteLine("book id = " + bookid);
+                Console.WriteLine("book title = " + title);
+                Console.WriteLine("book author= " + author);
+                Console.WriteLine("price = " + price);
+
+
+            }
+
+        }
+        public static void Main (String[] args)
+        {
+            book book = new book();
+            book.display();
+            Console.ReadLine();
+        }
+    }
+}
