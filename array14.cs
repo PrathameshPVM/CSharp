ca299eb232542203f7161471ebbd7a720e1fb7a8
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-25 14:42:38 +0530

    Add files via upload

---
+++ array14.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter no. of elements");
+            string s = Console.ReadLine();
+            int size = int.Parse(s);
+            long[] row = new long[size]; 
+
+            for (int i = 0; i < row.Length; i++)
+            {
+                Console.WriteLine("enter value");
+                row[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < row.Length; i++)
+            {
+
+                Console.WriteLine(" " + row[i]);
+
+            }
+
+
+        }
+
+    }
+}
