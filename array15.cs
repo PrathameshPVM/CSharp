5263979cbd9857ba78e5b25fb53b920514105a32
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-25 14:44:23 +0530

    Add files via upload

---
+++ array15.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            long[] row = new long[4] { 1, 2, 3, 4 };
+            long[] copy = row;
+
+            row[0]++;
+            long value = copy[0];
+            Console.WriteLine(value + "\n");
+
+
+            
+            for (int i = 0; i < copy.Length; i++)
+            {
+
+                Console.WriteLine(" " + copy[i]);
+
+            }
+
+
+        }
+
+    }
+}
