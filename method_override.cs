55d2f2f0e26dc129ad85cbc48931cd366603aa56
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-27 02:09:34 +0530

    Add files via upload

---
+++ method_override.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poly_1
+{
+    public class animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating......");
+        }
+    }
+    public class dog : animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating Bread.....");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            animal a = new dog();
+            a.eat();
+           
+        }
+    }
+}
+
