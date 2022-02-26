43e24bb4babd9c028bec146d9835d764928bf2e1
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-26 19:27:06 +0530

    Add files via upload

---
+++ inherit_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace inherit_1
+{
+    public class employee
+    {
+        public float salary = 40000;
+    }
+    public class programmer : employee
+    {
+        public float bonus = 1000;
+    }
+
+    public class program
+    {
+        public static void Main()
+        {
+            programmer p1 = new programmer();
+
+            Console.WriteLine("Salary: " + p1.salary);
+            Console.WriteLine("Bonus: " + p1.bonus);
+
+        }
+    }
+}
