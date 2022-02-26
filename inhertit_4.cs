e54f37b8214d7067559eef24e809b9aaf1426950
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-26 19:58:57 +0530

    Add files via upload

---
+++ inhertit_4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace inherit_4
+{
+    public class animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating..........");
+            
+        }
+    }
+    public class dog:animal
+    {
+        public void bark()
+        {
+            Console.WriteLine("Barking..........");
+        }
+    }
+    public class program
+    {
+        public static void Main()
+        {
+            dog d1 = new dog();
+
+            d1.eat();
+            d1.bark();
+        }
+        
+    }
+}
