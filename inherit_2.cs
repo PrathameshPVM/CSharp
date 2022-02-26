e5fe7d388d5102189d3ba122903100b52158fbc7
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-26 19:34:15 +0530

    Add files via upload

---
+++ inherit_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace inherit_2
+{
+    public class person
+    {
+        public String name;
+        public String address;
+    }
+    public class student: person
+    {
+        public int rno;
+        public int marks;
+    }
+    class program
+    {
+        public static void Main()
+        {
+            student p1 = new student();
+            p1.rno = 12;
+            p1.name = "amit";
+            p1.address = "Nagpur";
+            p1.marks = 75;
+
+            Console.WriteLine("rno: " + p1.rno);
+            Console.WriteLine("name: " + p1.name);
+            Console.WriteLine("address: " + p1.address);
+            Console.WriteLine("marks: " + p1.marks);
+        }
+    }
+}
