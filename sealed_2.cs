36b100af6a0caa2b2ad4b33df8fb05bebddb4707
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 01:40:28 +0530

    Add files via upload

---
+++ sealed_2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sealed_2
+{
+    sealed public class animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+    public class Dog : animal
+    {
+        public void bark() 
+        { 
+            Console.WriteLine("barking..."); 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.eat();
+            d.bark();
+
+        }
+    }
+}
+
