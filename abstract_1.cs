232052fd0496c19ace9bb445ae7aebaf31213e05
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-27 19:56:31 +0530

    Add files via upload

---
+++ abstract_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace abstract_1
+{
+    abstract class person
+    {
+        public abstract void display();
+
+        public void showdata()
+        {
+            Console.WriteLine("Show data non abstrsct method");
+        }
+    }
+    class student :person
+    {
+        public override void display()
+        {
+            Console.WriteLine("hello from display method");
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            //person person = new person();Error since person is abstract class
+            student s1 = new student();
+            s1.display();
+            s1.showdata();
+        }
+    }
+}
