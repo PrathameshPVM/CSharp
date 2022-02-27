fe0a007dc32691751c2b6f417e70413b3de94643
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-27 23:18:45 +0530

    Add files via upload

---
+++ interface_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface_2
+{
+    interface bank
+    {
+        String deposit(int actno, int amt);
+    }
+    class saving : bank
+    {
+        int actno;
+        int bal;
+        public string deposit(int actno,int amt)
+        {
+            this.actno = actno;
+            bal=bal+amt;
+            return "deposited successfully successfully ,bal is " + bal;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bank b = new saving();
+            string str = b.deposit(1, 200);
+            Console.WriteLine(str);
+        }
+    }
+}
+
