de81f26f0e7e21d49a25beab02fc849bb50e498a
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-27 23:01:07 +0530

    Add files via upload

---
+++ interface_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface_1
+{
+    public interface drawable
+    {
+        void draw();
+    }
+    public class reactangle:drawable
+    {
+        public void draw()
+        {
+            Console.WriteLine("drawing rectangle...");
+        }
+    }
+    public class circle : drawable
+    {
+        public void draw()
+        {
+            Console.WriteLine("drawing circle...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            drawable d = new reactangle();
+            d.draw();
+            drawable d2 = new circle();
+            d2.draw();
+        }
+    }
+}
+
