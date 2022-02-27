41ce20430a86fea943619dfc401634974f81fc7f
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-27 14:35:05 +0530

    Add files via upload

---
+++ poly_2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace poly_2
+{
+    public class animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating...........");
+        }
+    }
+    public class dog : animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating Bread..........");
+            //base.eat();
+        }
+    }
+    public class program
+    {
+        public static void Main()
+        {
+            animal a = new dog();
+            a.eat();
+        }
+    }
+}
