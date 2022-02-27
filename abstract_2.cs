4b1b1a514c8b6f451b689f9c0a98d0bf84e70223
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-27 20:05:54 +0530

    Add files via upload

---
+++ abstract_2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace abstract_2
+{
+    public abstract class shape
+    {
+        public abstract void draw();
+    }
+    public class rectangle:shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Rectangle..........");
+        }
+    }
+    public class circle :shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Circle..........");
+        }
+    }
+    public class program
+    {
+        public static void Main()
+        {
+            shape shape = new rectangle();
+            shape.draw();
+            shape shape1 = new circle();
+            shape1.draw();
+        }
+    }
+}
