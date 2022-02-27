eaa279635a1d105b13fe4a2b879029108ed29789
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-27 14:59:36 +0530

    Add files via upload

---
+++ poly_4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poly_4
+{
+    public class animal
+    {
+        public string color = "white";
+    }
+    public class dog : animal
+    {
+        public string color = "black";
+    }
+    public class program
+    {
+        public static void Main()
+        {
+            animal d= new dog();
+            Console.WriteLine(d.color);
+        }
+    }
+}
