513ca0d83c8226cae13fbce48e140c6efca1d3fb
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 01:35:15 +0530

    Add files via upload

---
+++ sealed_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sealed_1
+{
+    sealed class student
+    {
+        public string name;
+    }
+    //class parttime:student
+    //{    ERROR
+    
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
