0e204000f362a3ba81236c380e60c50d1d47351f
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 21:45:48 +0530

    Add files via upload

---
+++ arraylist_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraylist_1
+{
+
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add("Amit");
+            ar.Add("Rahul");
+            ar.Add("Raju");
+
+            foreach (string obj in ar)
+            {
+                Console.WriteLine("Student Name : "+obj);
+            }
+        }
+    }
+}
+
