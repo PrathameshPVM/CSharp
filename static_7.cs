c62243b1b919920c61870ee9ef0b9241843adc75
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 16:14:02 +0530

    Add files via upload

---
+++ static_7.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_7
+{
+    public class account
+    {
+        public int id;
+        public string name;
+        public static float roi;
+
+        public account (int id,string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        static account()
+        {
+            Console.WriteLine("static constructor is called ");
+            roi = 9.5f;
+
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
