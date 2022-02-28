55bb878a8ae30b9882002c3dbbe582dd57536956
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 15:12:15 +0530

    Add files via upload

---
+++ static_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sta
+{
+    class Program
+    {
+        static void display()
+        {
+            Console.WriteLine("hello");
+        }
+
+        static void Main(string[] args)
+        {
+            display();//to access static method display instance is not required
+        }
+    }
+}
+
