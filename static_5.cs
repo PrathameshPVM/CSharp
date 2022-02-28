76bebd83b4751d14ecb5162cb6e124de3aa1a266
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 15:54:31 +0530

    Add files via upload

---
+++ static_5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_5
+{
+    public class account
+    {
+        public int accno;
+        public string name;
+        public static int count=0;
+
+        public account (int accno,string name)
+        {
+            this.accno = accno;
+            this.name = name;
+            count++;
+        }
+        public void display()
+        {
+            Console.WriteLine(accno + " " + name);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            account a1 = new account(25, "raju");
+            account a2 = new account(289, "rahul");
+            account a3 = new account(152, "suresh");
+
+            a1.display();
+            a2.display();
+            a3.display();
+
+            Console.WriteLine("Total Object are :" + account.count);
+        }
+    }
+}
+
