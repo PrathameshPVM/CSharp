5d406323d81ff9b638b6428e4af46376ea0ba85b
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 17:55:05 +0530

    Add files via upload

---
+++ namespace_2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    public class hello
+    {
+        public void sayhello()
+        {
+            Console.WriteLine("Hello first namespace");
+        }
+    }
+    
+}
+namespace second
+{
+    public class hello
+    {
+        public void sayhello()
+        {
+            Console.WriteLine("Hello second namespace");
+        }
+    }
+}
+namespace namespace_2
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            first.hello h1 = new first.hello();
+            second.hello h2 = new second.hello();
+
+            h1.sayhello();
+            h2.sayhello();
+        }
+    }
+}
+
