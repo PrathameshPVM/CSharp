4e83b4e74e3cb791effb3b396a1b1830de7ee480
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 17:48:23 +0530

    Add files via upload

---
+++ namespace_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namespace_1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Namespace!");
+        }
+    }
+}
+
