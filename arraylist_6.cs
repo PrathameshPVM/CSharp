32f2582a8b7ae8bf9dde6ca78f5941164d6e66a4
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 22:28:02 +0530

    Add files via upload

---
+++ arraylist_6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraylist_6
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(19);
+            ar.Add(12);
+            ar.Add(13);
+            Console.WriteLine("array list colection without sorting");
+
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+            ar.Remove(13);
+            Console.WriteLine("array list colection after removing 13");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+            ar.RemoveAt(1);
+            Console.WriteLine("array list colection after removingat 1");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+
+        }
+    }
+}
+
