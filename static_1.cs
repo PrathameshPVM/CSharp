9565e35254f35bcfa8e3ecb6888ee49280afcb5b
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 15:08:05 +0530

    Add files via upload

---
+++ static_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_1
+{ 
+
+    class Program
+    {
+        void display()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.display();
+        }
+    }
+}
+
