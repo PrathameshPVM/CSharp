b76da5e8d734c79398a6f01566430d83d0b2380c
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 16:02:04 +0530

    Add files via upload

---
+++ static_6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_6
+{
+    public static class mymath
+    {
+        public static float pi = 3.14f;
+        public static int cube(int n)
+        {
+            return n * n * n;
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Value of PI :"+mymath.pi);
+            Console.WriteLine("Cube of 3 is :"+mymath.cube(3));
+        }
+    }
+}
+
