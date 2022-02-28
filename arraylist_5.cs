d622ebe9eff022c820d72a79a76fbf12773603dc
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 22:23:33 +0530

    Add files via upload

---
+++ arraylist_5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraylist_5
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(19);
+            ar.Add(12);
+            ar.Add(13);
+
+            Console.WriteLine("array list colection without sorting");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+
+            ar.Insert(1, 17);
+
+            Console.WriteLine("array list colection after adding element at 2 position");
+
+            foreach(int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+        }
+    }
+}
+
