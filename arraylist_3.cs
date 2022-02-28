208fde018f68d5e9e054b48aa13e82c8a9b30e32
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 22:07:40 +0530

    Add files via upload

---
+++ arraylist_3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraylist_3
+
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList a1 = new ArrayList();
+            a1.Add(25);
+            a1.Add(21);
+            a1.Add(3);
+
+            Console.WriteLine("array list collection without sorting");
+
+            foreach(int i in a1)
+            {
+                Console.WriteLine("Student Roll no"+i);
+            }
+            a1.Sort();
+            Console.WriteLine("array list collection with sorting");
+
+            foreach (int i in a1)
+            {
+                Console.WriteLine("Student Roll no :"+i);
+            }
+        }
+    }
+}
+
