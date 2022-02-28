938badfadf5821892ae875e7b972c72e1fb97b2b
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 01:16:10 +0530

    Add files via upload

---
+++ cl5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl5
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Enter any two number");
+            num1 =Convert.ToInt32(Console.ReadLine());
+            num2 =Convert.ToInt32(Console.ReadLine());
+
+            if(num1>num2)
+            {
+                Console.WriteLine("{0} is greater ", num1);
+            }
+            else
+            {
+                Console.WriteLine("{0} is greater ", num2);
+            }
+        }
+    }
+}
+
