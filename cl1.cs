59ec26de9439e80cd7a877cdd302b9969985afa1
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 00:46:41 +0530

    Add files via upload

---
+++ cl1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl1
+
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            float c, a, r;
+            r = 4.2f;
+            c = 2 * 3.14f * r;
+            a = 3.14f * r * r;
+
+            Console.WriteLine("Radius is {0} Circumferance is {1} and Area is {2} ",r,c,a);
+        }
+    }
+}
+
