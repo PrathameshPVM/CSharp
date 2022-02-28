ba5c00211442a7d6defbbd6bf779ca21796cf57f
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 00:55:34 +0530

    Add files via upload

---
+++ cl2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl2
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            long m, f, t;
+            float mp, fp;
+
+            Console.WriteLine("Enter total number of males and female");
+            m=Convert.ToInt64(Console.ReadLine());
+            f=Convert.ToInt64(Console.ReadLine());
+
+            t = m + f;
+
+            Console.WriteLine("Total number of males and female :"+t);
+
+            mp = m * 100 / t;
+            fp = m * 100 / t;
+
+            Console.WriteLine("Total Male Percentage is {0} and Total Female Percentage is {1}", mp, fp);
+        }
+    }
+}
+
