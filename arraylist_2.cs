f1c4d978c2a0a4d82afa7c903613e7e22fd1104f
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-02-28 22:01:59 +0530

    Add files via upload

---
+++ arraylist_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraylist_2
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(11);
+            ar.Add(12);
+            ar.Add(13);
+
+            foreach (int i in ar)
+            {
+                Console.WriteLine("Student RollNo :"+i);
+            }
+            Console.WriteLine("Capacity :"+ar.Capacity);
+            Console.WriteLine("Count :"+ar.Count);
+        }
+    }
+}
+
