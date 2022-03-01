e108c7e383abe499a9afbf597cada4cc04405ecd
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 18:41:28 +0530

    Add files via upload

---
+++ cl17.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl17
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            int num,i;
+
+            Console.WriteLine("Enter a number");
+            num=Convert.ToInt32(Console.ReadLine());
+
+            i = 2;
+            while (i<num-1)
+            {
+                if (num%i==0)
+                {
+                    Console.WriteLine("Not a prime no");
+                    break;
+                }
+                i++;
+            }
+            if (i==num)
+            {
+                Console.WriteLine("Prime Number");
+            }
+            
+
+
+        }
+    }
+}
+
