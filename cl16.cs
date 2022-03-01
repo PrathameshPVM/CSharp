6c8336983fe9c9d593b15784bca63f21decd577e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 18:28:07 +0530

    Add files via upload

---
+++ cl16.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl16
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            int i, j;
+            for ( i = 5; i <= 1; i++)
+            {
+                for ( j = 1; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine(" ");
+            }
+            for (i = 2; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
+
