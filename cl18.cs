06bc58bcb7bd9ca957cafd7a9d9cd526b32ac8c4
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 18:54:18 +0530

    Add files via upload

---
+++ cl18.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl18
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 20; i++)
+            {
+                for (j = 1; j <= 40 - i; j++)  
+                    
+                Console.Write(" ");
+
+                for (j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
+
