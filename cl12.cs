c002e98cb895f12168b0622c7897bb063cb6cc78
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 17:44:07 +0530

    Add files via upload

---
+++ cl12.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl12
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            int i, n, sign = 1;
+
+            Console.WriteLine("Enter the nth term");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            for ( i = 1; i < 2*n-1; i=i+2)
+            {
+                Console.WriteLine(i*sign);
+                sign=sign*(-i);
+            }
+            
+        }
+    }
+}
+
