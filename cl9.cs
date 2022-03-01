e92fef077d0577e680c3240b126c49967f628aa7
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 11:45:53 +0530

    Add files via upload

---
+++ cl9.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl8
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any colour code");
+            string code=Console.ReadLine();
+
+            switch (code)
+            {
+                case "blue":
+                Console.WriteLine("You Have Selected Blue");
+                
+                break;
+
+                case "green":
+                    Console.WriteLine("you have selected green");
+                    break ;
+
+                default:
+                    Console.WriteLine("Undifined Color");
+                    break;
+            }
+        }
+    }
+}
+
