dd0288f26e545738737624b3997da8297cd789c1
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 18:11:16 +0530

    Add files via upload

---
+++ cl15.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl15
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            int i, n = 0, g = 32768, b = 32767;
+            for (i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Enter a number");
+                n=Convert.ToInt32(Console.ReadLine());
+
+                if (n>g)
+                {
+                    g = n;
+                }
+                else if (n<b)
+                {
+                    b = n;
+                }
+                
+            }
+            Console.WriteLine("The greatest no = "+g);
+            Console.WriteLine("The smallest no = " + b);
+        }
+    }
+}
+
