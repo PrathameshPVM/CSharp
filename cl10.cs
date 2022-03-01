e710fc04c30c0cbf015fb642d33ca9ae6233aff5
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-01 11:54:10 +0530

    Add files via upload

---
+++ cl10.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl10
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Enter any Alphabet");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            switch (ch)
+            {
+                case 'a':
+                case 'A':
+                case 'e':
+                case 'E':
+                case 'i':
+                case 'I':
+                case 'o':
+                case 'O':
+                case 'u':
+                case 'U':
+                    Console.WriteLine("Entered Alphabet is VOWEL");
+                    break;
+
+                default:
+                    Console.WriteLine("Entered Alphabel is NOT VOWEL");
+                    break;
+            }
+        }
+    }
+}
+
