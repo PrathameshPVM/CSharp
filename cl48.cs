4a3585bdb1974915c80ecf5f0ce926d8ac0f88a9
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-02 22:26:40 +0530

    Add files via upload

---
+++ cl48.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 2,3 }, { 3, 4,5 }, { 5, 6,7 }, { 7, 8,8 }, { 9, 10 ,11} };
+            int i, j;
+
+
+            for (i = 0; i < 3; i++)
+            {
+
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("{0}",array[i, j]);
+                }
+            }
+
+        }
+    }
+}
+
