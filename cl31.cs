4815a3714e06ee4c7fe6a137c2a34f80c6a1897e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-02 15:08:54 +0530

    Add files via upload

---
+++ cl31.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int i, sum = 0;
+			int[] arr=new int[10];
+			
+			for (i = 0; i < 10; i++)
+			{
+                Console.WriteLine("Enter any number");
+				arr[i] = Convert.ToInt32(Console.ReadLine());
+				sum = sum + arr[i];
+			}
+
+            for (i = 0; i < arr.Length; i++)
+                Console.Write(" "+arr[i]);
+            Console.WriteLine("The sum of numbers {0}", sum);
+
+        }
+	}
+}
+
