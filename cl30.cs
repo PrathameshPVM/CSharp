94832dfc5ff451ed7d0d75b203b522eba62ca7b5
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-02 13:52:08 +0530

    Add files via upload

---
+++ cl30.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, sp = -1;
+			for (i = 5; i >= 1; i--)
+			{
+				for (j = 1; j <= i; j++)
+					Console.Write("*");
+				for (j = 1; j <= sp; j++)
+					Console.Write("_");
+				for (j = 1; j <= i; j++)
+					if (j != 5)
+						Console.Write("*");
+				Console.WriteLine("\n");
+				sp = sp + 2;
+			}
+
+		}
+	}
+}
+
