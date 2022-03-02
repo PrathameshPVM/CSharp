ee0003e494c649996424084c58d16c593e6c37fa
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-02 22:04:56 +0530

    Add files via upload

---
+++ cl43.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = { 22, 33, 44, 55, 66 };
+            
+            int sum = 0;
+            int i;
+            for (i = 0; i < 5; i++)
+            {
+                sum = sum + marks[i];
+
+            }
+            Console.WriteLine("Sum of Marks = "+sum);
+        }
+    }
+}
+
