2dff1cb11698eb83e387584c7208000390faffea
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-02 22:00:38 +0530

    Add files via upload

---
+++ cl42.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = { 22, 33, 44, 55, 66 };
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Marks = {0}", marks[i]);
+            }
+        }
+    }
+}
+
