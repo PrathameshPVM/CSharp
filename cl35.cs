b79ed49386756da7efedf86d8b6ae591f7e81f0b
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-02 15:42:39 +0530

    Add files via upload

---
+++ cl35.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl35
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3, 3];
+            int i;
+            for (i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Enter Name");
+                    a[i,j] = Convert.ToInt32(Console.ReadLine);
+                } 
+            }
+            for ( i = 0; i < 3; i++)
+            {
+                for (int j = 0; j <3; j++)
+                {
+                    Console.WriteLine(" "+a[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
