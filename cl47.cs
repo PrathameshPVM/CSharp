bc39cbcb1dfdcf5f93c354786ff124c00f91e9d1
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-02 22:21:54 +0530

    Add files via upload

---
+++ cl47.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 10 } };
+            int i, j;
+
+            
+            for (i = 0; i < 5; i++)
+            {
+
+                for (j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("a[{0}][{1}] = {2}\n", i, j, array[i,j]);
+                }
+            }
+
+        }
+    }
+}
+
