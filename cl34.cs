8802bc483f5eb921c343acdfae00aac8b41cfc46
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-02 15:29:13 +0530

    Add files via upload

---
+++ cl34.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname ;
+            string lname;
+            string name;
+
+
+            Console.WriteLine("Enter first name");
+            fname=Console.ReadLine();
+            Console.WriteLine("Enter last name");
+            lname = Console.ReadLine();
+            
+            Console.WriteLine("Name "+fname+" "+lname);
+
+        }
+    }
+}
+
