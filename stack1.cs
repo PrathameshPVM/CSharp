68d7b37a94b2de9335bca144aeba99d540502236
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-03 23:01:40 +0530

    Add files via upload

---
+++ stack1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace stack1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+            st.Push('B');
+            st.Push('A');
+            st.Push('C');
+            st.Push('M');
+            st.Push('G');
+            st.Push('W');
+
+            Console.WriteLine("Current Stack :");
+
+            foreach (char c in st)
+            {
+                Console.WriteLine(c+" ");
+            }
+            Console.WriteLine();
+
+            st.Push('V');
+            st.Push('H');
+
+            Console.WriteLine("The Next Poppable value in stack : {0}", st.Peek);
+            Console.WriteLine("Current stack: ");
+
+            foreach (char c in st)
+            {
+                Console.WriteLine(c+" ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Removing Value");
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            Console.WriteLine("Current stack: ");
+            foreach(char c in st)
+            {
+                Console.WriteLine(c+" ");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
