753c17dd4ca2a6c566ab455b98a955dfb8fa6dc3
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-04 01:18:33 +0530

    Add files via upload

---
+++ linked_list.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linked_list
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<int> nums = new LinkedList<int>();
+            nums.AddLast(23);
+            nums.AddLast(34);
+            nums.AddLast(33);
+            nums.AddLast(11);
+            nums.AddLast(6);
+            nums.AddFirst(9);
+            nums.AddFirst(7);
+
+            LinkedListNode<int> node = nums.Find(6);
+            nums.AddBefore(node, 5);
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
+
