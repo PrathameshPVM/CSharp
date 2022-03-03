7b2a6c784b43a4c241809e9a2f1c3a9bd99262c1
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-03 23:02:01 +0530

    Add files via upload

---
+++ stack2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack2
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Stack st = new Stack();
+
+            st.Push("Amar");
+            st.Push("Maria");
+            st.Push("Gomes");
+            st.Push("Wilson");
+            Console.WriteLine("Current stack: ");
+
+            foreach (string c in st)
+            {
+                Console.Write(c+" ");
+            }
+            Console.WriteLine();
+
+            st.Push("Vinita");
+            st.Push("Harshal");
+
+            Console.WriteLine("The next possible value in stack : {0}",st.Peek());
+            foreach (string c in st)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("The next poppable value in stack: {0}", st.Pop());
+            foreach (String c in st)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
+
