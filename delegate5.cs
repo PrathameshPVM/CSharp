d72f7054b1cd1909acc22f04d86f4b9187e2bf69
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-04 14:53:04 +0530

    Add files via upload

---
+++ delegate5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate5
+{
+   class publisher
+    {
+        public delegate int del1(int n1, int n2);
+
+        public event del1 event1;
+
+        public void raiseevent()
+        {
+            if (event1 != null)
+            {
+                event1(2, 3);
+            }
+            else
+            {
+                Console.WriteLine("No Subscriber");
+            }
+        }
+    }
+    public class program
+    {
+        public void addition(int n1,int n2)
+        {
+            int res = n1 + n2;
+            Console.WriteLine("Result : "+res);
+        }
+        public static void Main(string[] args)
+        {
+            publisher pb = new publisher();
+            pb.raiseevent();
+        }
+    }
+}
+
