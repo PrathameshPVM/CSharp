98f9c7ce8ad46eff80c91fa542eca5c286d4d61f
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-04 14:30:01 +0530

    Add files via upload

---
+++ delegate1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate1
+{
+    public class program
+    {
+        delegate void del_display();
+        public void display()
+        {
+            Console.WriteLine("hello form display method");
+        }
+        public static void Main(string[] args)
+        {
+            program p = new program();
+
+            del_display d1=new del_display(p.display);
+        }
+    }
+}
+
