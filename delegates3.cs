ed77ea226d0cd7112c3db023f6308a807a7724c2
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-04 14:40:08 +0530

    Add files via upload

---
+++ delegates3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace delegates3
+{
+    public delegate int del_square(int n1);
+
+    public class program
+    {
+        public int square(int n1)
+        {
+            return n1 * n1;
+        }
+        public static void Main(string[] args)
+        {
+            program p =new program();
+            del_square d1 = new del_square(p.square);
+                int res = d1(3);
+            Console.WriteLine("Square : "+res);
+        }
+    }
+}
+
