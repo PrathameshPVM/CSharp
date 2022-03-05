5116116a3dc9b2a5db1100899bb647a3f9306804
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-05 15:23:47 +0530

    Add files via upload

---
+++ cl61.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int anglea, angleb, anglec, sum;
+
+        Console.WriteLine("Input three angles of triangle : ");
+        anglea = Convert.ToInt32(Console.ReadLine());
+        angleb = Convert.ToInt32(Console.ReadLine());
+        anglec = Convert.ToInt32(Console.ReadLine());
+
+        // Calculate the sum of all angles of triangle 
+        sum = anglea + angleb + anglec;
+
+        // Check whether sum=180 then its a valid triangle otherwise invalid triangle
+        if (sum == 180)
+        {
+            Console.WriteLine("It is a valid triangle.\n");
+        }
+        else
+        {
+            Console.WriteLine("It is a invalid triangle.\n");
+        }
+
+        Console.ReadLine();
+    }
+}
