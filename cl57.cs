60cf4bca87b36db713915522f6814c5de3bb042e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-05 15:16:05 +0530

    Add files via upload

---
+++ cl57.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl57
+{
+    public class program
+    {
+        public static void Main(string[] args)
+
+
+        {
+            int Candiateage;
+
+            Console.WriteLine("Input the age of the candidate : ");
+            Candiateage = Convert.ToInt32(Console.ReadLine());
+
+            if (Candiateage < 18)
+            {
+                Console.WriteLine("Sorry, You are not eligible to caste your vote.\n");
+                Console.WriteLine(18 - Candiateage + "You would be able to caste your vote after %d year.\n");
+            }
+
+            else
+            {
+                Console.WriteLine("Congratulation! You are eligible for casting your vote.\n");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
