e9aef5ae3a06340eb6f5718df0f40cac09255209
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-05 14:29:04 +0530

    Add files via upload

---
+++ cl51.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl51
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            double n1, n2, res;
+
+            Console.WriteLine("Enter First number :");
+            n1= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second number :");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            res = n1 * n2;
+            Console.WriteLine(res);
+        }
+    }
+}
+
