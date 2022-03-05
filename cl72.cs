3ffb6ac2195ff0c3c13cd862f19dd6cff2cff50d
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-05 16:44:28 +0530

    Add files via upload

--- cl72.cs
+++ cl72.cs
@@ -2,22 +2,22 @@
 
 public class functionexcercise
 {
-    public static void interchangenumber(ref int num1, ref int num2)
+    public static bool checkprime(int num)
     {
-        int newnum;
-
-        newnum = num1;
-        num1 = num2;
-        num2 = newnum;
+        for (int i = 2; i < num; i++)
+            if (num % i == 0)
+                return false;
+        return true;
     }
     public static void Main()
     {
-        int n1, n2;
-        Console.Write("Enter two number: ");
-        n1 = Convert.ToInt32(Console.ReadLine());
-        n2 = Convert.ToInt32(Console.ReadLine());
-        interchangenumber(ref n1, ref n2);
-        Console.WriteLine("After swapping the 1st number is : {0} , and the 2nd number is : {1}", n1, n2);
+        Console.Write("Enter a number : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (checkprime(n))
+            Console.WriteLine(n + " is a prime number");
+        else
+            Console.WriteLine(n + " is not a prime number");
 
         Console.ReadLine();
     }
