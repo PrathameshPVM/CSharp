67aacdc946844bd331a81d37236df9c9102f3ef2
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-05 14:17:50 +0530

    Add files via upload

---
+++ prime_num.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_num
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int i, n;
+            Console.WriteLine("Enter number upto print prime num ");
+            n=Convert.ToInt32(Console.ReadLine());
+            for (i = 2; i <= n; i++)
+            {
+                if (i%2!=0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+        
+    }
+}
