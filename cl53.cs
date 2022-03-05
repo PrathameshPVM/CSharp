1837a06184307a88d7bc21135450568954c2c3c1
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-05 14:34:00 +0530

    Add files via upload

---
+++ cl53.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl53
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            double meter, feet;
+
+            Console.WriteLine("Enter feet :");
+            feet = Convert.ToDouble(Console.ReadLine());
+
+            meter = feet / 3.2808399;
+
+            Console.WriteLine(meter);
+        }
+    }
+}
+
