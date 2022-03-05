40474bafb8a9f1cc92ab4b685930bb74f86c66ad
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-05 14:41:29 +0530

    Add files via upload

---
+++ cl55.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cl55
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        
+            
+            {
+                int number;
+                Console.WriteLine("Enter a number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("Entered Number is an Even Number");
+                }
+
+                else
+                {
+                    Console.WriteLine("Entered Number is odd Number");
+                }
+                    
+
+                Console.ReadLine();
+            }
+        
+    }
+}
+
