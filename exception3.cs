cbe0491fbf514cd42fb8133b7a037452316c9aa2
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-07 18:46:00 +0530

    Add files via upload

---
+++ exception3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception3
+{ 
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            int num;
+            int n;
+
+            Console.WriteLine("Enter number first");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number second");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int res = 0;
+            try
+            {
+                res = num / n;
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine("Connot Divide by Zero");
+            }
+            Console.WriteLine("Res = " + res);
+            Console.WriteLine("Bye");
+        }
+    }
+}
+
