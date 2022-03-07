edc4562232555abcf088de9e51ebc6d2063d6030
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-07 16:35:58 +0530

    Add files via upload

---
+++ exception1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception1
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            int num = 10;
+            int n = 0;
+            int res = num / n;
+            Console.WriteLine("Res = "+res);
+            Console.WriteLine("Bye");
+        }
+    }
+}
+
