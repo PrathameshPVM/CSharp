ef33d26e7899316e56c278fa51483dc352efea60
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-09 00:25:28 +0530

    Add files via upload

---
+++ thread1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thread1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread th = Thread.CurrentThread;
+            th.Name = "MainThread";
+            Console.WriteLine("This is {0} ",th.Name);
+            Console.ReadLine();
+        }
+    }
+}
+
