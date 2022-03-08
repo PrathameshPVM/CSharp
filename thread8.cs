262e7d5c6f949c705a4b815744ac6b1cddde381e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-09 01:46:16 +0530

    Add files via upload

---
+++ thread8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thread8
+{
+    public class program
+    {
+        public static void display()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("i = " + i);
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            ThreadStart ts = new ThreadStart(display);
+            Thread t1 = new Thread(display);
+            Thread t2 = new Thread(display);
+
+            t1.Start();
+            t1.Join();
+            t2.Start();
+        }
+    }
+}
+
