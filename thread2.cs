dd3831f04f6b32e091c6d51b8e76d174abc18224
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-09 00:57:43 +0530

    Add files via upload

---
+++ thread2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thread2
+{
+    public class program
+    {
+        public static void CallToChildThread()
+        {
+            Console.WriteLine("Child Thread Start");
+        }
+        public static void Main(string[] args)
+        {
+            ThreadStart Childref = new ThreadStart(CallToChildThread);
+            Console.WriteLine("In Main : Creating the child thread");
+
+            Thread childThread=new Thread(Childref);
+            childThread.Start();
+            Console.ReadLine();
+        }
+    }
+}
+
