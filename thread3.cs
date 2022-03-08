b5de6a632772f7d59b0255e81424d60d76effa86
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-09 01:01:45 +0530

    Add files via upload

---
+++ thread3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thread3
+{
+    public class program
+    {
+        public void display()
+        {
+            for (int i = 0; i <= 25; i++)
+            {
+                Console.WriteLine("i = "+i);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            program p = new program();
+            Thread t1 = new Thread(p.display);
+            Thread t2 = new Thread(p.display);
+            Thread t3 = new Thread(p.display);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
+
