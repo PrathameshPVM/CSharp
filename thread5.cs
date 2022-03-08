5c85ed8ea794e57d66f0e7a19831a4cd7ebb14fe
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-09 01:13:31 +0530

    Add files via upload

---
+++ thread5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thread5
+{
+    public class program
+    {
+        public static void CallToChildThraed()
+        {
+            Console.WriteLine("Child Thread Starts");
+            int sleepfor = 5000;
+
+            Console.WriteLine("Child Thread Paused for {0} seconds", sleepfor / 1000);
+            Thread.Sleep(sleepfor);
+            Console.WriteLine("Child thread resumes");
+        }
+
+        public static void Main(string[] args)
+        {
+            ThreadStart childref = new ThreadStart(CallToChildThraed);
+            Console.WriteLine("In Main: Creating the Child thread");
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+            Console.ReadKey();
+
+        }
+    }
+}
+
